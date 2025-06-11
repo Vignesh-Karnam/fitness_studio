54e31e72b714b78412a66464d684cfac4e7d35be
VigneshKaranam <vignesh.k@blooprint.in>
2025-06-11 22:20:04 +0530

    added time zone

--- fitness_booking/fitness_booking/settings.py
+++ fitness_booking/fitness_booking/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -122,10 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-TIME_ZONE = 'Asia/Kolkata'
-USE_TZ = True
 
 LOGGING = {
     'version': 1,
